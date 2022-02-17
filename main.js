24b5ff88772ce660a8434c42043f31cb0fb3d992
aline_who <alinemontes93@gmail.com>
2022-02-17 08:53:27 -0600

    Se agregó el archivo index.html al repositorio

---
+++ main.js
@@ -0,0 +1,22 @@
+//Escribe el código en javascript para:
+
+let suma;
+let x = prompt("Declara la variable x");
+let y = prompt("Declara la variable y");
+suma = parseFloat(x) + parseFloat(y);
+    console.log("La suma es " + suma);
+
+//Escribe la variable para:
+
+let nombre = "Aline";
+console.log(nombre);
+let 
+
+/*Guardar tu edad
+Guardar tu nombre
+Guardar tu dirección.
+Guardar el precio de un bolillo
+Guardar la fecha de nacimiento
+Guardar tu clave de elector
+Guardar el nombre de tu mascota*/
+
